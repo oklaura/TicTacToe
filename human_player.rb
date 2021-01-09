ba16f06bbba75d00c6f3453d1307a55d7a6b108b
“mrsDARCY” <l.kress@yahoo.com>
2021-01-09 18:44:42 -0500

    get_position row column text

--- human_player.rb
+++ human_player.rb
@@ -8,7 +8,7 @@ class HumanPlayer
 
     def get_position
         puts "Player #{@mark}-"
-        puts "Enter two numbers representing row(1-3) and column(1-3)"
+        puts "Enter two numbers representing row(0-2) and column(0-2)"
         puts "in the format 'row col' i.e. 1 2"
         pos = gets.chomp.split(" ").map(&:to_i)
         raise "invalid position" if pos.length != 2
