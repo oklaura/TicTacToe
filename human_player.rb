05787a31c2068259ceffe3e89136829b8167af21
“mrsDARCY” <l.kress@yahoo.com>
2021-01-26 13:47:46 -0500

    get position edge cases

--- human_player.rb
+++ human_player.rb
@@ -7,11 +7,13 @@ class HumanPlayer
     end
 
     def get_position(arr)
+        pos = nil
+        until arr.include?(pos)
         puts "Player #{@mark}-"
         puts "Enter two numbers representing row(0-2) and column(0-2)"
         puts "in the format 'row col' i.e. 1 2"
         pos = gets.chomp.split(" ").map(&:to_i)
-        raise "invalid position" if pos.length != 2
+        end
         pos
     end
 
