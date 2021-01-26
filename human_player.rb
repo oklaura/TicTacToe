0d3a94f7d8c133336149e641fdb0092f9d127289
“mrsDARCY” <l.kress@yahoo.com>
2021-01-26 13:59:16 -0500

    Invalid Position Error Message

--- human_player.rb
+++ human_player.rb
@@ -13,6 +13,11 @@ class HumanPlayer
         puts "Enter two numbers representing row(0-2) and column(0-2)"
         puts "in the format 'row col' i.e. 1 2"
         pos = gets.chomp.split(" ").map(&:to_i)
+        if !arr.include?(pos)
+            puts
+            puts "-- Error: Invalid position, try again --"
+            puts
+        end
         end
         pos
     end
